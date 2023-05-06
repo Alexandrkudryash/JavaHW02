b8b22dce1b06487a2c34a76ee37ab547cfeee54d
alex21008 <alexandrkudryash@mail.ru>
2023-05-06 11:28:23 +0300

    created new Task

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+/*
+Домашнее задание 2, предмет - знакомство с языком программирования Java
+Задача 1.
+Напишите программу, чтобы найти наименьшее окно в строке, содержащей все символы другой строки
+*/
+
+
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("Hello world!");
+        System.out.println("Hello the biutifull world again!!!");
+
+    }
+
+    static void searchStr(String s1, String s2){
+        String[] arr1 = s1.sp(" ");
+
+
+
+    }
+
+
+}
